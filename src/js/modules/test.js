6983efb86da4213e5ca5a9318e11a76638b3fd19
Hakan Bilgin <hbi99@hotmail.com>
2024-10-12 21:17:56 +0200

    more level maps

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -3,7 +3,7 @@ let Test = {
 	init(APP) {
 		// return;
 
-		APP.edit.dispatch({ type: "render-level", arg: "f" });
+		APP.edit.dispatch({ type: "render-level", arg: "i" });
 		// APP.edit.dispatch({ type: "output-pgn" });
 
 	}
