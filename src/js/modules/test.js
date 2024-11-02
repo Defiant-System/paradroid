192a79301bac915febb318e8de4e1dc9d8fa5b63
Hakan Bilgin <hbi99@hotmail.com>
2024-11-02 10:13:00 +0100

    updated console coords

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -18,14 +18,14 @@ let Test = {
 		/*
 		 * EDITOR
 		APP.dispatch({ type: "show-view", arg: "editor" });
-		APP.editor.dispatch({ type: "render-level", arg: "1" });
-		return;
-		 */
+		APP.editor.dispatch({ type: "render-level", arg: "18" });
 
-		// APP.editor.els.palette.find(`.tab-row span:nth(2)`).trigger("click");
+		APP.editor.els.palette.find(`.tab-row span:nth(2)`).trigger("click");
 		// APP.editor.els.palette.find(`.tiles[data-click="select-action-tile"] b[data-size="2x2"]`).trigger("click");
 		// // APP.editor.els.palette.find(`.buttons span[data-arg=".25"]`).trigger("click");
 		// // APP.editor.dispatch({ type: "output-pgn" });
+		return;
+		 */
 
 		// // setTimeout(() => APP.editor.els.palette.find(`.buttons span[data-click="output-collision-pgn"]`).trigger("click"), 300);
 		// return;
