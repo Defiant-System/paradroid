38343cd84e338f4ab12263a72ee2c48d970233bd
Hakan Bilgin <hbi99@hotmail.com>
2024-11-08 18:39:04 +0100

    Update door.js

--- src/js/classes/door.js
+++ src/js/classes/door.js
@@ -26,6 +26,33 @@ class Door {
 	}
 
 	render(ctx) {
-		
+		let arena = this.arena,
+			viewport = arena.viewport,
+			tile = arena.config.tile,
+			x = ((this.x - .5) * tile) + viewport.x,
+			y = ((this.y - .5) * tile) + viewport.y,
+			frame = 128 + (this.frame.index * 64),
+			args = [arena.assets["big-map"].img, frame, 256, 64, 64, 0, 0, 64, 64],
+			isVert = this.type === "v";
+
+		// frames for vertical door
+		if (isVert) args[2] = 320;
+
+		ctx.save();
+		ctx.translate(x, y);
+
+		// normal draw if debug mode is < 3
+		if (arena.debug.mode < 3) {
+			ctx.drawImage(...args);
+		}
+
+		// if debug mode on, draw extras
+		// if (arena.debug.mode > 0) {
+		// 	args = isVert ? [0, 16, 64, 32] : [16, 0, 32, 64];
+		// 	ctx.fillStyle = "#00000066";
+		// 	ctx.fillRect(...args);
+		// }
+
+		ctx.restore();
 	}
 }
