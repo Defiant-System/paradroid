8fcc86aadccbc1803e819dae6bd22e1659057e07
Hakan Bilgin <hbi99@hotmail.com>
2024-11-10 23:23:46 +0100

    console view

--- src/js/areas/console.js
+++ src/js/areas/console.js
@@ -16,12 +16,31 @@
 			el;
 		// console.log(event);
 		switch (event.type) {
+			// system events
+			case "window.keydown":
+				let active = Self.els.el.find(".option.active"),
+					options = Self.els.el.find(".option"),
+					index = active.index();
+				switch (event.char) {
+					case "return":
+						break;
+					case "up":
+						active.removeClass("active");
+						index = Math.max(index - 1, 0);
+						options.get(index).addClass("active");
+						break;
+					case "down":
+						active.removeClass("active");
+						index = Math.min(index + 1, options.length-1);
+						options.get(index).addClass("active");
+						break;
+				}
+				break;
 			// custom events
 			case "select-view":
 				Self.els.el.find(".option.active").removeClass("active");
 				// make lift active
 				el = $(event.target).addClass("active");
-				
 				console.log( el.data("view") );
 				break;
 		}
