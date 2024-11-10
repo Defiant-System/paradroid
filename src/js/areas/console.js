85387d5a088a91d679bfc3fe3f7df9bb41dc7593
Hakan Bilgin <hbi99@hotmail.com>
2024-11-10 22:31:14 +0100

    console view; active option UI / UX

--- src/js/areas/console.js
+++ src/js/areas/console.js
@@ -6,6 +6,7 @@
 		// fast references
 		this.els = {
 			content: window.find("content"),
+			el: window.find(".console-view"),
 		};
 	},
 	dispatch(event) {
@@ -16,7 +17,12 @@
 		// console.log(event);
 		switch (event.type) {
 			// custom events
-			case "put-tile":
+			case "select-view":
+				Self.els.el.find(".option.active").removeClass("active");
+				// make lift active
+				el = $(event.target).addClass("active");
+				
+				console.log( el.data("view") );
 				break;
 		}
 	}
