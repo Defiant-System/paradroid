f7416da15a78a5f7f0f6aeb62fe6ac0775336e8a
Hakan Bilgin <hbi99@hotmail.com>
2024-11-13 23:03:58 +0100

    level 1 walls

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -5,7 +5,7 @@ let Test = {
 
 		// return APP.dispatch({ type: "show-view", arg: "terminated" });
 		// return APP.dispatch({ type: "show-view", arg: "start" });
-		return APP.dispatch({ type: "show-view", arg: "console" });
+		// return APP.dispatch({ type: "show-view", arg: "console" });
 
 		/*
 		 * Finder algorithm
@@ -30,7 +30,6 @@ let Test = {
 
 		/*
 		 * EDITOR
-		 */
 		APP.dispatch({ type: "show-view", arg: "editor" });
 		APP.editor.dispatch({ type: "render-level", arg: "1" });
 
@@ -46,6 +45,7 @@ let Test = {
 
 		// // setTimeout(() => APP.editor.els.palette.find(`.buttons span[data-click="output-collision-pgn"]`).trigger("click"), 300);
 		// return;
+		 */
 
 
 		return setTimeout(() => {
@@ -53,7 +53,7 @@ let Test = {
 					// map: { id: 1, clear: .15 }, player: { id: "001", x: 25, y: 9, power: .25 }, debug: { mode: 1 },
 					// map: { id: 2, clear: .25 }, player: { id: "302", x: 35, y: 15, power: .35 }, debug: { mode: 0 },
 					// map: { id: 11, clear: .35 }, player: { x: 5, y: 3, power: .35 }, debug: { mode: 0 },
-					map: { id: 1, clear: .1 }, player: { x: 5, y: 7, power: .5 }, debug: { mode: 1 },
+					map: { id: 1, clear: .1 }, player: { x: 23, y: 9, power: .5 }, debug: { mode: 1 },
 					// map: { id: 4, clear: .1 }, player: { x: 33, y: 5, power: .25 }, debug: { mode: 0 },
 					// map: { id: 3, clear: .5 }, player: { x: 19, y: 15, power: .5 }, debug: { mode: 0 },
 				};
