261a775c7c2486b99f79db94efc9b779adb467b7
Hakan Bilgin <hbi99@hotmail.com>
2024-11-16 23:52:50 +0100

    more blueprints

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -24,7 +24,7 @@ let Test = {
 		 * Console View
 		 */
 		APP.dispatch({ type: "show-view", arg: "console" });
-		APP.console.dispatch({ type: "show-droid", value: "598" });
+		APP.console.dispatch({ type: "show-droid", value: "821" });
 		return;
 
 		/*
