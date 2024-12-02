41ca5ee064e8d743646143c17380276e1d9e68a9
Hakan Bilgin <hbi99@hotmail.com>
2024-12-02 23:36:56 +0100

    testing missile configs

--- src/js/classes/missile.js
+++ src/js/classes/missile.js
@@ -5,44 +5,57 @@ class Missile {
 
 		this.arena = arena;
 		this.owner = owner;
-		this.target = new Point(target.x, target.y);
-		
-		this.bullet = Math.random();
 		this.angle = angle + Math.PI / 2;
 		this.asset = arena.assets.missile;
-		this.trail = [];
-		
+		this.bullet = Math.random();
+
 		this.position = owner.position.clone();
 		this.position.x += Math.cos(angle) * 30;
 		this.position.y += Math.sin(angle) * 30;
 
-		let speed = .00001,
+		this.target = new Point(target.x, target.y);
+
+		let speed = .0000025,
 			vX = Math.cos(angle) * speed,
  			vY = Math.sin(angle) * speed;
 		this.velocity = new Point(vX, vY);
-		// this.velocity = new Point(0, 0);
-		this.maxSpeed = .001;
-		this.maxForce = .00001;
+	    this.acceleration = new Point(0, 0);
+
+	    this.maxspeed = .001;
+	    this.maxforce = .000025;
 
 		this.body = Matter.Bodies.circle(this.position.x, this.position.y, 2, { frictionAir: .006 });
 		this.body.label = `fire-${this.bullet}`;
 
 		// add to map entries
 		this.arena.map.addItem(this);
-
-		this.update(16);
 	}
 
-	seek(target) {
-		let force = target.subtract(this.position);
-		force = force.setMagnitude(this.maxSpeed);
-		force = force.subtract(this.velocity);
-		force = force.limit(this.maxForce);
-		return force;
+	destroy() {
+		let arena = this.arena,
+			owner = this.owner,
+			index = arena.map.entries.indexOf(this);
+		arena.map.entries.splice(index, 1);
+		// remove from physical world
+		Matter.Composite.remove(arena.map.engine.world, this.body);
+		// sparks at kill zone
+		new Sparks({ arena, owner, x: this.position.x, y: this.position.y });
 	}
 
-	move(force) {
-		Matter.Body.applyForce(this.body, this.body.position, force);
+	seek() {
+		let target = this.target;
+		let desired = target.subtract(this.position);
+		desired = desired.setMagnitude(this.maxspeed);
+		let steer = desired.subtract(this.velocity);
+	    steer = steer.limit(this.maxforce);
+	    this.applyForce(steer);
+	}
+
+	applyForce(force) {
+		this.acceleration = this.acceleration.add(force);
+		Matter.Body.applyForce(this.body, this.body.position, this.acceleration);
+
+		this.angle = this.velocity.direction();
 
 		// copy physical position to "this" internal position
 		this.position.x = this.body.position.x;
@@ -50,14 +63,18 @@ class Missile {
 	}
 
 	update(delta) {
-		let force = this.velocity.setMagnitude(delta/16);
-		// let force = this.seek(this.target);
-		this.move(force);
-
-		// prepend position to trail
-		this.trail.unshift({ x: this.position.x, y: this.position.y });
-		// trim trail log
-		this.trail.splice(9, 9);
+		if (this.position.distance(this.target) < 15) {
+			return this.destroy();
+		} else if (this.position.distance(this.owner.position) < 40) {
+			let force = this.velocity.setMagnitude(.5);
+			this.applyForce(force);
+		} else {
+			this.seek();
+			this.velocity = this.velocity.add(this.acceleration).limit(this.maxspeed);
+			this.position = this.position.add(this.velocity);
+			this.acceleration = this.acceleration.multiply(0);
+			// this.angle = this.target.subtract(this.position).direction();
+		}
 
 		// update tile position
 		let tile = this.arena.config.tile;
