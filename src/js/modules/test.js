9f97c22baf79fdc5ba6a8417b20cd5fcecade381
Hakan Bilgin <hbi99@hotmail.com>
2024-12-21 23:37:22 +0100

    svg explosion sprite sheet

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -33,7 +33,6 @@ let Test = {
 		
 		/*
 		 * TRANSFER View
-		 */
 		APP.dispatch({ type: "show-view", arg: "transfer" });
 		setTimeout(() => APP.transfer.dispatch({ type: "render-schemas" }), 50);
 		// setTimeout(() => APP.transfer.dispatch({ type: "generate-schemas" }), 50);
@@ -58,6 +57,7 @@ let Test = {
 			// APP.transfer.AI.setOrder([2, 6]);
 		}, 1000);
 		return;
+		 */
 
 
 		/*
