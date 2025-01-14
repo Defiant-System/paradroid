520a0cae23449f7ab5c275fb43cc2b5a1f101c83
Hakan Bilgin <hbi99@hotmail.com>
2025-01-14 16:14:49 +0100

    ui improvements

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -58,6 +58,7 @@ let Test = {
 
 		/*
 		 * TRANSFER View
+		 */
 		APP.dispatch({ type: "show-view", arg: "transfer", anim: "none" });
 		setTimeout(() => APP.transfer.dispatch({ type: "render-schemas" }), 50);
 		// setTimeout(() => APP.transfer.dispatch({ type: "generate-schemas" }), 50);
@@ -68,10 +69,10 @@ let Test = {
 		// setTimeout(() => APP.transfer.els.cpu.data({ winner: "flicker" }), 250);
 		// setTimeout(() => APP.transfer.chooseColor = true, 150);
 
-		let el = APP.transfer.els.cbLeft.find(`.toggler`);
-		setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 2 }), 150);
-		setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 4 }), 350);
-		return;
+		// let el = APP.transfer.els.cbLeft.find(`.toggler`);
+		// setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 2 }), 150);
+		// setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 4 }), 350);
+		// return;
 
 		// setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 7 }), 1100);
 		// setTimeout(() => APP.transfer.dispatch({ type: "toggle-io-row", el, index: 6 }), 1100);
@@ -85,7 +86,6 @@ let Test = {
 			// APP.transfer.AI.setOrder([2, 6]);
 		}, 1000);
 		return;
-		 */
 
 
 		/*
